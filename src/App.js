5ffead5fab1d39132619ce2097c89588bd6e7261
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-06 20:40:38 -0500

    scrapped app.js

--- src/App.js
+++ src/App.js
@@ -22,57 +22,8 @@ class App extends React.Component {
      }
   }
 
-
-
   
-  render() { 
-    return ( 
-      <Router>
-
-        <div className="weatherbox">
-          <ul>
-            <li>
-              {/* describes what thelink looks likes */}
-              <Link to="/">Day</Link>
-              <p> { this.state.city} </p>
-            </li>
-          </ul>
-
-            {/* switch */}
-            {/* "path" mathces the "to" in the link */}
-            {/* route mathces to the component you have */}
-          <Switch>
-            <Route path="/">
-              {/* returns day functions */}
-
-              <Day />
-            </Route>
-          </Switch>
-        </div>
-
-      </Router>
-    );
-  }
   async Day () {
     const weatherDay = await axios.get('http://api.openweathermap.org/data/2.5/forecast?q=Dallas,us&APPID=a3f2467086e2bd2e13ffdad5355858ba');
     this.setState({ city: weatherDay.data})
   }
-
-
-}
-
-function Day(){
-  let getToday = useRouteMatch();
-  
-  return ( 
-    <div>
-      <h2>Day of the Week Temp {this.state.city.map.temp}</h2>
-
-      {/* {this.state.data.map((recipe, id) => <li key={id}> {recipe.title} </li>)} */}
-
-    </div>
-  )
-}
-
-
-export default App;
