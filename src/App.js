aae6ea9dcd790716bd798c2227493bc90268624e
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-06 21:55:07 -0500

    added getWEather to return to test

--- src/App.js
+++ src/App.js
@@ -37,7 +37,11 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="weatherbox">
-        <p id="render-weatherbox">15deg</p>
+        <p id="render-weatherbox" >
+          {/* .this refers to the getWeather */}
+          {/* prop={value} */}
+          getWeather={this.getWeather}
+          15deg</p>
         <p id="render-weatherbox">80deg</p>
         <p id="render-weatherbox">30deg</p>
         <p id="render-weatherbox">50deg</p>
