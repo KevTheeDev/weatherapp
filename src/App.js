ccbcce6635747725491d18d22be0a087e9f98b41
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-06 21:00:43 -0500

    added p tag render-weatherbox

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="weatherbox">
-        <p></p>
+        <p id="render-weatherbox"></p>
       </div>
     )
   }
