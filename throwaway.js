e563467e1ff12907c1a18f5948666beaf59b45e0
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-06 20:39:59 -0500

    added throwaway.js / moved majoiryt of arleady made code into it

---
+++ throwaway.js
@@ -0,0 +1,39 @@
+// {/* <div className="weatherbox">
+//           <ul>
+//             <li>
+//               {/* describes what thelink looks likes */}
+//               <Link to="/">Day</Link>
+//               <p> { this.state.city} </p>
+//             </li>
+//           </ul>
+
+//             {/* switch */}
+//             {/* "path" mathces the "to" in the link */}
+//             {/* route mathces to the component you have */}
+//           <Switch>
+//             <Route path="/">
+//               {/* returns day functions */}
+
+//               <Day />
+//             </Route>
+//           </Switch>
+//         </div>
+
+//       </Router>
+//     );
+//   }
+
+
+// }
+
+// function Day(){
+//   let getToday = useRouteMatch();
+  
+//   return ( 
+//     <div>
+//       <h2>Day of the Week Temp {this.state.city.map.temp}</h2>
+
+//     </div>
+//   )
+// }
+//  */}
